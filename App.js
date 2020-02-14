b9791df3e325112591dca6e5e200549f6e296e3f
ReinaldoDEV <61064330+ReinaldoDEV@users.noreply.github.com>
2020-02-14 17:45:38 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Header from './src/Componentes/Header';
+import PeopleList from './src/Componentes/PeopleList';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peoples: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?nat=br&results=5').then(response => {
+      const {results} = response.data;
+      this.setState({
+        peoples: results,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas" />
+        <PeopleList peoples={this.state.peoples} />
+      </View>
+    );
+  }
+}
